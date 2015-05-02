bf8e49e55b739049d369f20047ffc9695cf6eeb7
berowar <berowar@gmail.com>
2015-05-02 11:17:48 +0300

    Revert "don’t write audit for already destroyed objects"
    This reverts commit bcad7d5093339b0124d3e0a7eb189f4728eb007c.

--- lib/audited/auditor.rb
+++ lib/audited/auditor.rb
@@ -197,7 +197,7 @@ module Audited
 
       def audit_destroy
         write_audit(:action => 'destroy', :audited_changes => audited_attributes,
-                    :comment => audit_comment) if self.persisted?
+                    :comment => audit_comment) unless self.new_record?
       end
 
       def write_audit(attrs)
